d51d8b338c4974edf8e3fe0cfe58650a6a0d05dc
Miheer Vaidya <m.vaidya@utah.edu>
2021-12-03 23:45:49 -0700

    Hello True False

--- hello/tests/cli.rs
+++ hello/tests/cli.rs
@@ -1,7 +1,19 @@
 use assert_cmd::Command;
 
 #[test]
-fn runs() {
-    let mut cmd = Command::cargo_bin("hello").unwrap();
+fn run_true() {
+    let mut cmd = Command::cargo_bin("true").unwrap();
     cmd.assert().success();
+}
+
+#[test]
+fn run_false() {
+    let mut cmd = Command::cargo_bin("false").unwrap();
+    cmd.assert().failure();
+}
+
+#[test]
+fn run_hello() {
+    let mut cmd = Command::cargo_bin("hello").unwrap();
+    cmd.assert().success().stdout("Hello, World!\n");
 }
